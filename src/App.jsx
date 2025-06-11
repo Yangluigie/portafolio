3a81bf9c6cb7542d44c80bc0285cf09a499f8062
yang florido solano <yang1995@outlook.es>
2025-06-11 15:18:34 -0500

    importar el i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
-import i18n from "./i18next";
+import i18n from "./i18n";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
